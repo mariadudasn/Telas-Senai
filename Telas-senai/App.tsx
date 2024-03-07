f464ebf918c34f56b2031095fdadd1cc4b126298
mariadudasn <maria.santos081207@gmail.com>
2024-03-07 15:56:52 -0300

    Update scanner

--- Telas-senai/App.tsx
+++ Telas-senai/App.tsx
@@ -4,13 +4,14 @@ import Cadastro from './screens/Cadastro'
 import Inventário from './screens/Inventário';
 import Cadastro_de_itens from './screens/Cadastro_de_itens'
 import Descrição_de_itens from './screens/Descrição_de_itens';
+import Scanner from './screens/Scanner';
 
 
 
 export default function App() {
   return (
     <>
-        <Descrição_de_itens></Descrição_de_itens>
+        <Scanner></Scanner>
     </>
   );
 }
