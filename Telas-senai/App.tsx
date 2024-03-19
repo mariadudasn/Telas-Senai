807e05e9592da112e31bd0fbf40dbb06e3a98e10
mariadudasn <maria.santos081207@gmail.com>
2024-03-19 13:22:51 -0300

    Update editar

--- Telas-senai/App.tsx
+++ Telas-senai/App.tsx
@@ -6,13 +6,14 @@ import Cadastro_de_itens from './screens/Cadastro_de_itens'
 import Descrição_de_itens from './screens/Descrição_de_itens';
 import Scanner from './screens/Scanner';
 import Load from './screens/Load';
+import Editar from './screens/Editar';
 
 
 
 export default function App() {
   return (
     <>
-        <Load></Load>
+        <Editar></Editar>
     </>
   );
 }
---
+++ Telas-senai/screens/Editar.tsx
@@ -0,0 +1,36 @@
+import {View, StyleSheet, ScrollView} from 'react-native';
+import Nav from '../components/Nav';
+import { AntDesign } from '@expo/vector-icons';
+import InputInv from '../components/InputInv';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import Footer from '../components/Footer';
+
+export default function Cadastro_de_itens() {
+  return (
+    <>
+      <View style={stylesCadInv.container}>
+        <Nav corr="#FFFFFF" backcolor="#FF0000" icone={<AntDesign name="arrowleft" size={30} color="#FFFFFF" />} texto="Editar" icone2={<AntDesign name="checkcircle" size={30} color="#FFFFFF" />} />
+        <ScrollView  style={stylesCadInv.container2}>
+          <InputInv title="N° Inventário"/>
+          <InputInv title="Descrição"/>
+          <InputInv title="Local"/>
+          <InputInv title="Responsável"/>
+          <InputInv title="Data de Registro"/>
+        </ScrollView>
+      </View>
+      <Footer bordacolor= "#CBCBCB" corr="#595959" colorr="#FFFFFF" icone={<MaterialIcons name="flip-camera-ios" size={30} color="#595959" />} icone2={<Fontisto name="list-1" size={25} color="#595959" />}/>
+    </>
+  );
+}
+
+const stylesCadInv = StyleSheet.create({
+  container:{
+    flex: 1
+  },
+
+  container2:{
+    paddingTop: 30,
+  },
+
+})
