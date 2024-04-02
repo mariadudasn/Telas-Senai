c6365542b54835178b1202db31cfb9810e70e7b9
mariadudasn <maria.santos081207@gmail.com>
2024-04-02 15:30:25 -0300

    Update navegacao_telasiniciais

--- Telas-senai/app/_layout.tsx
+++ Telas-senai/app/_layout.tsx
@@ -3,18 +3,9 @@ import { Stack } from 'expo-router';
 export default function layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{
-        headerShown: false}} />
-      <Stack.Screen name="TelasIniciais/Recusenha" options={{
-        headerTitle: "",
-        headerTransparent: true,
-        headerTintColor:"#FFFFFF"
-      }} />
-      <Stack.Screen name="TelasIniciais/Cadastro" options={{
-        headerTitle: "",
-        headerTransparent: true,
-        headerTintColor:"#FFFFFF"
-      }} />
+      <Stack.Screen name="index" options={{headerShown: false, statusBarColor:"#000000"}} />
+      <Stack.Screen name="TelasIniciais/Recusenha" options={{headerTitle: "", statusBarColor:"#011E83", headerTransparent: true, headerTintColor:"#FFFFFF"}} />
+      <Stack.Screen name="TelasIniciais/Cadastro" options={{headerTitle: "", headerTransparent: true, headerTintColor:"#FFFFFF", statusBarColor:"#FF0000"}} />
     </Stack>
   );
 }
