eecdb1219c99dbd4974c5a1b4c1ce13c065f174e
mariadudasn <maria.santos081207@gmail.com>
2024-04-25 13:54:42 -0300

    Update customdawer

--- Telas-senai/components/CustomDrawer.tsx
+++ Telas-senai/components/CustomDrawer.tsx
@@ -6,7 +6,7 @@ export default function CustomDrawer(props:any){
     const cores = useColor()
     return (
         <View style={{ paddingTop:60, backgroundColor: cores.bgPrimary, height: "100%"}}>
-            <View style={{padding:20, display: "flex", flexDirection: "row"}}>
+            <View style={{padding:20, display: "flex", flexDirection: "row", borderBottomWidth: 1, borderBottomColor: "#D9D9D9"}}>
             <Image source={require('@assets/Flavio.png')}/>
                 <View style={{marginLeft: 15, marginTop: 18}}>
                     <Text style={{fontWeight: "bold", fontSize: 16, color: cores.txtcolorPrimary}}>Flavio Josefo</Text>
